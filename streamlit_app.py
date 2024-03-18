a82959e19edcbd35d3fc364af414b705d1a70ff5
Liuhugos <162983384+Liuhugos@users.noreply.github.com>
2024-03-18 12:59:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,9 +14,11 @@ st.write(indices)
 word_fr=voc["Definition"].values[j]
 st.write("Traduis;"+word_fr)
 for i in range(4):
-  st.button(voc["Hanzi"].values[indices[i]],on_clitch=is_correct,args=(indices[i],j))
+  st.button(voc["Hanzi"].values[indices[i]],on_click=is_correct,args=(indices[i],j))
 def is_correct(i,j):
   if i==j:
-    st.write("Bravo")
-             else:
-               st.write("Raté")
+      st.write("Bravo")
+  else:
+    st.write("Raté")
+  
+            
